427a6e3f124d94bee0862aa77efa66812fc98a26
rithik-06 <rithiktiwari33@gmail.com>
2026-01-25 02:25:36 +0530

    data

--- src/data/fetch_stock_data.py
+++ src/data/fetch_stock_data.py
@@ -3,3 +3,25 @@ fetch stock data from yahoo finance
 this scripts downloads historical stock prices
 """
 
+import yfinance as yf
+import pandas as pd
+from pathlib import Path
+from datetime import datetime
+
+def fetch_stock_data(ticker: str ,period: str = "5y", output_dir: str= None):
+    """
+    Args:
+     ticker: Stock symbol (e.g., 'AAPL', 'GOOGL')
+        period: Time period ('1d', '5d', '1mo', '1y', '5y')
+        output_dir: Directory to save the CSV file
+
+    :return:
+    dataframe with stock data
+    """
+
+    print(f"fetching data for {ticker}...")
+
+    try:
+
+
+
